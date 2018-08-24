1afac7d382713ef1a127a3046b4a4d568fa5073f
DESKTOP-EBNN66H\sif <rakeshvasal@cardinbox.com>
2018-08-24 19:20:56 +0530

    reactive intro done

--- app/src/main/java/com/example/sif/mvptest/ReactiveTestActivity.java
+++ app/src/main/java/com/example/sif/mvptest/ReactiveTestActivity.java
@@ -3,12 +3,26 @@ package com.example.sif.mvptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.sif.mvptest.Model.DataModels.Note;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.SchedulerSupport;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class ReactiveTestActivity extends AppCompatActivity {
@@ -17,38 +31,134 @@ public class ReactiveTestActivity extends AppCompatActivity {
 
     private Disposable disposable;
 
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    private CompositeDisposable customDisposable = new CompositeDisposable();
+
+    LinearLayout parent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reactive_test);
-
+        parent = findViewById(R.id.parent);
         Observable<String> animalsObservable = getObservables();
 
-        Observer<String> animalsObserver = getAnimalsObserver();
+        DisposableObserver<String> animalsObserver = getAnimalsObserver();
 
-        animalsObservable
+        DisposableObserver<String> allcapsObserver = getAllCapsObserver();
+
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("b");
+                            }
+                        })
+                        .subscribeWith(animalsObserver));
+
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("c");
+                            }
+                        })
+                        .map(new Function<String, String>() {
+
+                            @Override
+                            public String apply(String s) throws Exception {
+                                return s.toUpperCase();
+                            }
+                        })
+                        .subscribeWith(allcapsObserver));
+
+
+        customDisposable.add(getCustomObjectObservable()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .filter(new Predicate<String>() {
+                .map(new Function<Note, Note>() {
                     @Override
-                    public boolean test(String s) throws Exception {
-                        return s.toLowerCase().startsWith("b");
+                    public Note apply(Note note) throws Exception {
+
+                        note.setNote(note.getNote().toUpperCase());
+                        return note;
                     }
                 })
-                .subscribe(animalsObserver);
-
+                .subscribeWith(getCustomObjectObserver()));
     }
 
-    private Observer<String> getAnimalsObserver() {
-        return new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                disposable = d;
-                Log.d(TAG, "onSubscribe");
-            }
+    private DisposableObserver<String> getAllCapsObserver() {
+        return new DisposableObserver<String>() {
 
             @Override
             public void onNext(String s) {
+                addTextView(s);
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
+    private DisposableObserver<Note> getCustomObjectObserver() {
+        return new DisposableObserver<Note>() {
+            @Override
+            public void onNext(Note o) {
+                addTextView(o.getNote());
+                Log.d(TAG, "Name: " + o.getNote());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All notes are emitted!");
+            }
+        };
+    }
+
+    private Observable<Note> getCustomObjectObservable() {
+        final List<Note> notes = prepareNotes();
+
+        return Observable.create(new ObservableOnSubscribe<Note>() {
+            @Override
+            public void subscribe(ObservableEmitter<Note> emitter) throws Exception {
+                for (Note note : notes) {
+                    if (!emitter.isDisposed()) {
+                        emitter.onNext(note);
+                    }
+                }
+                if (!emitter.isDisposed()) {
+                    emitter.onComplete();
+                }
+            }
+        });
+    }
+
+    private DisposableObserver<String> getAnimalsObserver() {
+        return new DisposableObserver<String>() {
+
+            @Override
+            public void onNext(String s) {
+                addTextView(s);
                 Log.d(TAG, "Name: " + s);
             }
 
@@ -73,10 +183,30 @@ public class ReactiveTestActivity extends AppCompatActivity {
                 "Fox", "Frog");
     }
 
+
+    private List<Note> prepareNotes() {
+        List<Note> notes = new ArrayList<>();
+        notes.add(new Note(1, "buy tooth paste!"));
+        notes.add(new Note(2, "call brother!"));
+        notes.add(new Note(3, "watch narcos tonight!"));
+        notes.add(new Note(4, "pay power bill!"));
+
+        return notes;
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // don't send events once the activity is destroyed avoid memory leaks'
-        disposable.dispose();
+        compositeDisposable.clear();
+    }
+
+    private void addTextView(String text) {
+        ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        TextView tv = new TextView(this);
+        tv.setLayoutParams(lparams);
+        tv.setText(text);
+        this.parent.addView(tv);
     }
 }
